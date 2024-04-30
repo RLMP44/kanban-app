9d7a52ee92d0ed75641f8c6a57cf3a67d8b6de1e
Rachael Momber <rlmomber@gmail.com>
2024-04-30 21:44:38 +0900

    setup experiment

--- app/javascript/initSortable.js
+++ app/javascript/initSortable.js
@@ -7,6 +7,30 @@ const initKanbanSortable = (ulElements) => {
       animation: 300
     });
   });
+
+  let column1 = document.getElementById('column1');
+  let column2 = document.getElementById('column2');
+  let aisle1 = document.getElementById('aisle1');
+
+  new Sortable(column1, {
+    animation: 150,
+    group: 'kanban',
+    // ghostClass: 'sortable-ghost',
+  });
+
+  new Sortable(column2, {
+    animation: 150,
+    group: 'kanban',
+    // ghostClass: 'sortable-ghost',
+    // sort: false
+  });
+
+  new Sortable(aisle1, {
+    animation: 150,
+    group: 'kanban',
+    // ghostClass: 'sortable-ghost',
+  });
 };
+
 
 export { initKanbanSortable }
